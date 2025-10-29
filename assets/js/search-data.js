ee15b9061d993cfa53e5962d1892b168932dc24e
sukchanlee1 <scleev@snu.ac.kr>
2025-10-29 07:18:51 +0000

    Deploying to gh-pages from @ sukchanlee1/sukchanlee1.github.io@4d0a33e144f1fafc51b7f4988989a9a374c46e5a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/research/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/repositories/";
+            window.location.href = "/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
