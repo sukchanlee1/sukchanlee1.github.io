d9fd07b2c4825402d62bbddea404d93e31b7570b
sukchanlee1 <scleev@snu.ac.kr>
2025-10-29 04:11:14 +0000

    Deploying to gh-pages from @ sukchanlee1/sukchanlee1.github.io@4725483abca1eaf3ad170f65ac4876e64ff18d96 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+  },{id: "nav-research",
+          title: "research",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
+            window.location.href = "/research/";
           },
         },{id: "post-a-post-with-plotly-js",
         
